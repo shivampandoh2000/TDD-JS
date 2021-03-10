512b1f6c2481ae18a88cfb37dff078b2616f3315
Shivam Pandoh <shivampandoh@Shivams-MacBook-Air.local>
2021-03-10 13:27:03 +0530

    +1 div test case

--- div.test.js
+++ div.test.js
@@ -14,6 +14,10 @@ test("When numbers are 2 and 0", () => {
   expect(c.divide(2, 0)).toBe(Infinity);
 });
 
+test("when numbers are Infinity and Infinity", () => {
+  expect(c.divide(Infinity, Infinity)).toBe(NaN);
+});
+
 test("When the numbers are 2*10^9 and 10^9", () => {
   let actual = c.divide(2 * Math.pow(10, 9), 1 * Math.pow(10, 9));
   expect(actual).toBe(2);
